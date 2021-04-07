df1732601e62286a2b20380028ce16e4aa7f200b
Helium-Tang <343711936@qq.com>
2021-04-07 09:46:13 +0800

    add e2e test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     'jest/globals': true,
+    'cypress/globals': true,
     node: true,
     commonjs: true,
   },
@@ -14,7 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
     'no-console': 'off',
     indent: ['error', 2],
